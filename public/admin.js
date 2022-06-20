20c903f9833da0d41dfe38efa92e1a1f0a57b58e
Owen Andre <owencandre@gmail.com>
2022-06-20 14:30:36 -0400

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,41 @@
+// const { json } = require("body-parser")
 
 // Your Code Here
+const main = async () => {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json();
+    books.forEach(updateBookNumber);
+}
+
+
+const updateBookNumber = (book) => {
+    let root = document.querySelector('#root')
+
+    let bookLi = document.createElement('li')
+    bookLi.textContent = book.title + ': '
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    bookLi.append(quantityInput, saveButton)
+
+    root.append(bookLi)
+}
+
+main();
